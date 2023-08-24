43c9dd228f944c0855906e5898e36b5545c8ec7e
ishanka158 <96714@fhss.sjp.ac.lk>
2023-08-24 22:54:23 +0530

    initial component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,58 @@
 import Home from "./pages/home/Home"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
+import Users from "./pages/users/Users.";
+import Product from "./pages/products/Product";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
 
 function App() {
 
+  const Layout = ()=>{
+    return(
+      <div className="main"> 
+        <Navbar/>
+        <div className="container">
+            <div className="menuContainer"><Menu/></div>
+            <div className="contentContainer"><Outlet/></div>
+        </div>
+        <Footer/>
+      </div>
+    )
+  }
+
+
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      children:[
+        {
+          path:"/",
+          element:<Home/>
+        },
+        {
+          path:"/users",
+          element:<Users/>
+        },
+        {
+          path:"/products",
+          element:<Product/>
+        }
+      ]
+    },
+  ]);
+  
+ 
+
   return (
-    <div><Home></Home></div>
+    <RouterProvider router={router}></RouterProvider>
   )
 }
 
