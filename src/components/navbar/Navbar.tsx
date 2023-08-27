980cd3f3e04567f0ea7e35d9383722581c875b0d
ishanka158 <96714@fhss.sjp.ac.lk>
2023-08-27 14:03:15 +0530

    component added

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,41 @@ import "./navbar.scss"
 
 const Navbar = ()=>{
     return (
-        <div className="navbar">Navbar</div>
+        <div className="navbar">
+            <div className="logo">
+
+                <img src="logo.svg" alt=""/>
+                <span>ADMIN</span>
+
+            </div>
+            <div className="icons">
+
+                <img src="/search.svg" alt="" className="icon" />
+                <img src="/app.svg" alt="" className="icon" />
+                <img src="/expand.svg" alt="" className="icon" />
+
+                <div className="notification">
+
+                    <img src="/notifications.svg" alt="" className="icon" />
+                    <span>1</span>
+
+                </div>
+                <div className="user"> 
+                    
+                    <img src="src\components\images\profile.jpg" 
+                    alt=" " />
+                    <span>Madhuranga</span>
+                    
+                </div>    
+
+                <img src="/settings.svg" alt="" className="icon" />
+
+
+
+
+            </div>
+
+        </div>
     )
 }
 
