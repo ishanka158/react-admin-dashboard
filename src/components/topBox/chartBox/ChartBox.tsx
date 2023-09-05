d99f3b0931c3962daa138ea2b3979be47c6a452c
ishanka158 <96714@fhss.sjp.ac.lk>
2023-09-05 23:35:31 +0530

    props added

--- src/components/topBox/chartBox/ChartBox.tsx
+++ src/components/topBox/chartBox/ChartBox.tsx
@@ -1,19 +1,48 @@
+import {Link} from "react-router-dom";
 import "./chartBox.scss";
+import { ResponsiveContainer, LineChart, Line, Tooltip } from "recharts";
 
-const ChartBOx = () => {
+type props = {
+  color:string;
+  icon:string;
+  title:string;
+  datakey:number | string;
+  percentage:number;
+  ChartData:object[];
+}
+
+const ChartBOx = (props: Props) => {
     return (
         
         <div className="chartBox">
 
             <div className="boxInfo">
                 <div className="title">
-                    <img src="/user.svg" alt=""></img>
-                    <span>Total Users</span>
+                    <img src={props.icon} alt=""></img>
+                    <span>{props.title}</span>
+                </div>
+                <h1>{props.number}</h1>
+                <Link to="/" style={{color:props.color}}>View all</Link>
+            </div>
+            <div className="chartInfo">
+                <div className="chart">
+                <ResponsiveContainer width="99%" height="100%">
+                    
+                    <LineChart data={props.chartData}>  
+                    <Tooltip
+                    contentStyle={{background:"transparent", border:"none"}}
+                    labelStyle={{display:"none"}}
+                    position={{x:10, y:60}}
+                    />
+                    <Line type="monotone" dataKey={props.dataKey} stroke={props.color} strokeWidth={2}  dot={false}/>
+                    </LineChart>
+                </ResponsiveContainer>
+                </div>
+                <div className="texts">
+                    <span className="percentage" style={{color: props.percentage < 0 ? "tomato":"limegreen"}}>{props.percentage}%</span>
+                    <span className="duration">this month</span>
                 </div>
             </div>
-            <div className="chartInfo"></div>
-
-
         </div>
     )
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import "./Home.scss";
 import TopBox from "../../components/topBox/TopBox";
 import ChartBOx from "../../components/topBox/chartBox/ChartBox";
+import { chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from "../../data";
 
 const Home = () =>{
     return(
@@ -8,10 +9,10 @@ const Home = () =>{
             <div className="box box1"><TopBox/>
                 
             </div>
-            <div className="box box2"><ChartBOx/></div>
-            <div className="box box3"><ChartBOx/></div>
-            <div className="box box4"><ChartBOx/></div>
-            <div className="box box5"><ChartBOx/></div>
+            <div className="box box2"><ChartBOx {...chartBoxUser}/></div>
+            <div className="box box3"><ChartBOx {...chartBoxProduct}/></div>
+            <div className="box box4"><ChartBOx {...chartBoxRevenue}/></div>
+            <div className="box box5"><ChartBOx {...chartBoxConversion}/></div>
             <div className="box box6">Box6</div>
             <div className="box box7">Box7</div>
             <div className="box box8">Box8</div>
